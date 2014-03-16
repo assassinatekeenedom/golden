3e3fabaf808f8c0cbfeb6add49482c76208d6822
rnkeene <rnkeene@gmail.com>
2014-03-15 21:53:51 -0400

    log4j is becoming more stable

---
+++ bin/action/repo/git/.git.bat
@@ -0,0 +1,1 @@
+call git clone %1 %2
--- pojo/src/main/java/com/golden/FileAppender.java
+++ pojo/src/main/java/com/golden/FileAppender.java
@@ -6,17 +6,24 @@ import java.io.IOException;
 
 public enum FileAppender {
 
-    APACHE("C:/golden/bin/httpd/conf/httpd.conf");
+    APACHE("C:/golden/conf/httpd.conf");
 
-    private String path;
+    private File local;
 
     FileAppender(String path) {
-        this.path = path;
+        try {
+            this.local = new File(path);
+            local.delete();
+            local.createNewFile();
+        } catch (IOException ex) {
+            System.out.println("Error Writing/Appending to output File");
+        }
     }
 
     public void append() {
         try {
-            FileWriter writer = new FileWriter(new File(this.path), true);
+
+            FileWriter writer = new FileWriter(local, true);
             writer.append("\nListen 80");
             writer.append("\nNameVirtualHost *:80");
             writer.append("\n<VirtualHost *:80>");
